3c634b302c142ba42dcc83afafcea65b0c6a4ab1
Yetity <hlonipoole692@gmail.com>
2023-04-15 15:48:48 +0200

    FUCKING PAIN

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, IntentsBitField, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ActivityType } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler')
 
 const client = new Client({
     intents: [
@@ -10,88 +11,6 @@ const client = new Client({
     ]
 });
 
-let status = [
-    {
-        name: "the Gulag",
-        type: ActivityType.Competing
-    },
-    {
-        name: "wtf by Prod.$elly",
-        type: ActivityType.Listening
-    },
-    {
-        name: "Crab Game on Steam",
-        type: ActivityType.Playing
-    }
-]
-
-client.on('ready', (c) => {
-    console.log(`${c.user.tag} is online`);
-
-    setInterval(() => {
-        let random = Math.floor(Math.random() * status.length)
-        client.user.setActivity(status[random])
-    }, 10000)
-})
-
-client.on('messageCreate', (message) => {
-    if (message.author.bot) {
-        return
-    }
-
-    if (message.content === 'button') {
-        const row = new ActionRowBuilder();
-        row.components.push(
-            new ButtonBuilder().setCustomId('button').setLabel("button").setStyle(ButtonStyle.Primary)
-        )
-        message.channel.send({
-            content: 'some content',
-            components: [row]
-        })
-    }
-})
-
-client.on('interactionCreate', (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'add') {
-        const num1 = interaction.options.get('num1').value;
-        const num2 = interaction.options.get('num2').value;
-        const ans = num1 + num2
-
-        interaction.reply(`${num1} + ${num2} = ${num1 + num2}`)
-    }
-
-
-    if (interaction.commandName === 'embed') {
-	const embed = new EmbedBuilder()
-        .setTitle("Title")
-        .setDescription("description lmao");
-	const embed2 = new EmbedBuilder()
-	    .setTitle("yeye")
-	    .setDescription("yeyeyeye")
-        .setColor('Random')
-        .setFields(
-            {
-                name: "field",
-                value:"field value",
-                inline: true
-            },
-            {
-                name:"anotherr field?!?!?",
-                value: "yeah fr fr",
-                inline: false
-            }
-        )
-        
-        interaction.reply({ embeds: [embed, embed2] })
-    }
-})
-
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isButton()) return;
-    await interaction.deferReply({ephemeral: true});
-    await interaction.editReply("ok")
-})
+eventHandler(client);
 
 client.login(process.env.TOKEN);

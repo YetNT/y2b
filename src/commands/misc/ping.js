253660ffd9814e7ae7f811deeded5de562f66ed1
Yetity <hlonipoole692@gmail.com>
2023-05-01 11:06:06 +0200

    cleansed

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,3 +1,5 @@
+const { /*PermissionFlgasBits,*/ Client, Interaction } = require('discord.js')
+
 module.exports = {
   name: 'ping',
   description: 'Pong!',
@@ -5,7 +7,16 @@ module.exports = {
   testOnly: true,
   // options: Object[],
   // deleted: Boolean,
+  // cooldown: 10000,
+  // blacklist: true,
+  // permissionsRequired: [PermissionFlagsBits.Administrator],
+  // botPermissions: [PermissionFlagsBits.Administrator],
 
+  /**
+   * 
+   * @param {Client} client 
+   * @param {Interaction} interaction 
+   */
   callback: (client, interaction) => {
     interaction.reply(`Pong! ${client.ws.ping}ms`);
   },

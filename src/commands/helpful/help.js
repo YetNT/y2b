cd1d70b75f4704184e1facba660a8361bd1e6087
Yetity <hlonipoole692@gmail.com>
2023-05-06 17:43:26 +0200

    Help with select menu

--- src/commands/helpful/help.js
+++ src/commands/helpful/help.js
@@ -1,8 +1,8 @@
-const { Client, Interaction, EmbedBuilder } = require('discord.js')
 
-let names = ['daily', 'rob', 'work', 'balance', 'deposit', 'withdraw', 'help', 'ping']
-var desc = [];
-let fields = [];
+const { Client, Interaction, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js')
+
+let eco = ['daily', 'rob', 'work', 'balance', 'deposit', 'withdraw']
+let other = ['help', 'ping']
 
 module.exports = {
     name: "help",
@@ -17,27 +17,93 @@ module.exports = {
         try {
             await interaction.deferReply();
             const commands = await client.application?.commands.fetch();
-            names.forEach((n) => {
-                let r = commands.find(l => l.name === n);
-                desc.push(r.description);
+
+            const select = new StringSelectMenuBuilder()
+                .setCustomId('help')
+                .setPlaceholder('Pick a category')
+                .addOptions(
+                    new StringSelectMenuOptionBuilder()
+                        .setLabel('Economy')
+                        .setDescription('All the economy commands')
+                        .setValue('eco')
+                        .setEmoji(':money_with_wings:'),
+                    new StringSelectMenuOptionBuilder()
+                        .setLabel('Others')
+                        .setDescription('The other commands')
+                        .setValue('other')
+                        .setEmoji(':information_source:'),
+                );
+
+            const row = new ActionRowBuilder()
+                .addComponents(select);
+
+            await interaction.editReply({
+                content: 'help',
+                components: [row],
             });
 
-            await new Promise((resolve) => {
-                for (let i = 0; i < names.length; i++) {
-                    fields.push({
-                        name: names[i],
-                        value: desc[i]
-                    });
+            client.on('interactionCreate', async (interaction) => {
+                if (interaction.isStringSelectMenu() && interaction.customId === 'help') {
+                    const selectedOption = interaction.values[0];
+                    var message = await interaction.message.fetch();
+                    let r; // get slash command from cache
+
+                    if (selectedOption === 'eco') {
+                        let ecoDesc = []
+                        let ecoId = []
+                        let ecoFields = []
+
+                        eco.forEach((n) => {
+                            r = commands.find(l => l.name === n);
+                            ecoId.push(r.id)
+                            ecoDesc.push(r.description);
+                        });
+            
+                        await new Promise((resolve) => {
+                            for (let i = 0; i < eco.length; i++) {
+                                ecoFields.push({
+                                    name: `**</${eco[i]}:${ecoId[i]}>**`,
+                                    value: ecoDesc[i]
+                                });
+                            }
+                            resolve();
+                        });
+
+                        await message.edit({ content: "h", components: [row], embeds: [new EmbedBuilder().setTitle("Economy").setFields(ecoFields).setColor("#4FC5E3")] });
+                    }
+                    if (selectedOption === 'other') {
+                        let otherDesc = []
+                        let otherId = []
+                        let otherFields = []
+
+                        other.forEach((n) => {
+                            r = commands.find(l => l.name === n);
+                            otherId.push(r.id)
+                            otherDesc.push(r.description);
+                        });
+            
+                        await new Promise((resolve) => {
+                            for (let i = 0; i < other.length; i++) {
+                                otherFields.push({
+                                    name: `**</${other[i]}:${otherId[i]}>**`,
+                                    value: otherDesc[i]
+                                });
+                            }
+                            resolve();
+                        });
+
+                        await message.edit({ content: "h", components: [row], embeds: [new EmbedBuilder().setTitle("Others").setFields(otherFields).setColor("#4FC5E3")] });
+                    }
+                   interaction.deferUpdate()
+                    if (message) {
+                        setTimeout(async () => {
+                            message.edit({components:[]});
+                        }, 30000);
+                    } else {
+                        // do nothing.
+                    }
+                    
                 }
-                resolve();
-            });
-
-            interaction.editReply({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle("help")
-                        .setFields(fields)
-                ]
             });
         } catch (error) {
             console.log(error);

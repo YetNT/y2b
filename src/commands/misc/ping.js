15dc36f52e9f4d17684f91fd4a9ee1883298e9ca
Yetity <hlonipoole692@gmail.com>
2023-05-06 11:49:44 +0200

    advanced ping

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,4 +1,4 @@
-const { /*PermissionFlgasBits,*/ Client, Interaction } = require('discord.js')
+const { /*PermissionFlgasBits,*/ Client, Interaction, EmbedBuilder } = require('discord.js')
 
 module.exports = {
   name: 'ping',
@@ -17,7 +17,32 @@ module.exports = {
    * @param {Client} client 
    * @param {Interaction} interaction 
    */
-  callback: (client, interaction) => {
-    interaction.reply(`Pong! ${client.ws.ping}ms`);
+  callback: async (client, interaction) => {
+    try {
+    const sent = await interaction.reply({ embeds: [new EmbedBuilder().setDescription('Pinging...')], fetchReply: true });
+    interaction.editReply({
+      embeds : [
+        new EmbedBuilder()
+          .setTitle("Pong!")
+          .setFields(
+            [
+              {
+                name:"Roundtrip latency",
+                value:`${sent.createdTimestamp - interaction.createdTimestamp}ms`,
+                inline: true
+              },
+              {
+                name:"Websocket heartbeat",
+                value: `${client.ws.ping}ms.`,
+                inline: true
+              }
+            ]
+          )
+        .setColor("Green")
+      ]
+    });
+    } catch (error) {
+     console.log(error) 
+    }
   },
 };

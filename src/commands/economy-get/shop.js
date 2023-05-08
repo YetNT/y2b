43d6d7ff323eaf559fd9b5014ff60abf6f34d369
Yetity <hlonipoole692@gmail.com>
2023-05-08 19:05:17 +0200

    shop

---
+++ src/commands/economy-get/shop.js
@@ -0,0 +1,30 @@
+const { Client, Interaction, EmbedBuilder } = require('discord.js')
+const items = require('../../utils/items/items.json')
+const [ comma, coin, shopify ] = require('../../utils/beatify')
+
+module.exports = {
+    name:"shop",
+    description:"Shop for items that you (tottaly) can afford",
+    blacklist: true,
+    
+    callback: async (client, interaction) => {
+        try {
+            await interaction.deferReply()
+            let reply = ``;
+
+            for (let item in items) {
+                reply += `__${items[item].name}__ - ${shopify(items[item].price)}\n`;
+            }
+            console.log(reply)
+
+            await interaction.editReply({ embeds : [
+                new EmbedBuilder()
+                    .setTitle("Shop")
+                    .setDescription(reply)
+                    .setColor("Fuchsia")
+            ] })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}

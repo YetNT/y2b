2f82c3e28e9da1cdba75f31142611858f414b32b
Yetity <hlonipoole692@gmail.com>
2023-05-13 17:35:32 +0200

    Added commands to help

--- src/commands/helpful/help.js
+++ src/commands/helpful/help.js
@@ -1,7 +1,7 @@
 
 const { Client, Interaction, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js')
 
-let eco = ['daily', 'rob', 'work', 'balance', 'deposit', 'withdraw', 'buy', 'promocode', 'share', 'shop']
+let eco = ['daily', 'rob', 'work', 'balance', 'deposit', 'withdraw', 'buy', 'promocode', 'share', 'shop', 'inventory', 'leaderboard']
 let other = ['help', 'ping']
 
 module.exports = {

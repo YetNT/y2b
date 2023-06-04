426f28a94180b7b434f574fb6f5977bc664a5869
Yetity <hlonipoole692@gmail.com>
2023-06-04 12:48:55 +0200

    if < switch/case

--- src/utils/strToMilli.js
+++ src/utils/strToMilli.js
@@ -1,9 +1,36 @@
+/**
+ * @param {String} input Input
+ */
 module.exports = (input) => {
-    var ar = input.match(/[a-zA-Z]+|[0-9]+/g)
-    if (ar[1] == "s" || ar[1] == "seconds") { return +ar[0] * 1000; }
-    if (ar[1] == "min" || ar[1] == "minutes" || ar[1] == "mins") { return +ar[0] * 60000; } 
-    if (ar[1] == "h" || ar[1] == "hour" || ar[1] == "hours") { return +ar[0] * 3.6e+6; } 
-    if (ar[1] == "d" || ar[1] == "day" || ar[1] == "days") { return +ar[0] * 8.64e+7; } 
-    if (ar[1] == "w" || ar[1] == "week" || ar[1] == "weeks") { return +ar[0] * 6.048e+8; }
-    if (ar[1] == "m" || ar[1] == "month" || ar[1] == "months") { return +ar[0] * 2.628e+9; }
-}
+    var ar = input.match(/[a-zA-Z]+|[0-9]+/g);
+    var value = +ar[0];
+    var unit = ar[1];
+  
+    switch (unit) {
+        case "s":
+        case "seconds":
+            return value * 1000;
+        case "min":
+        case "minutes":
+        case "mins":
+            return value * 60000;
+        case "h":
+        case "hour":
+        case "hours":
+            return value * 3.6e+6;
+        case "d":
+        case "day":
+        case "days":
+            return value * 8.64e+7;
+        case "w":
+        case "week":
+        case "weeks":
+            return value * 6.048e+8;
+        case "m":
+        case "month":
+        case "months":
+            return value * 2.628e+9;
+        default:
+            return 0; // Invalid input, return 0 or handle the error as desired
+    }
+};

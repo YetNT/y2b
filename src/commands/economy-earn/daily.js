e9b9af124918d032b68f6fd4ee84a7edca48a0f5
Yetity <hlonipoole692@gmail.com>
2023-06-08 17:41:02 +0200

    formatted my code after 5432452 years
    i was playing around and actually got my code formatter to work :skull:

--- src/commands/economy-earn/daily.js
+++ src/commands/economy-earn/daily.js
@@ -1,57 +1,68 @@
-const { EmbedBuilder } = require('discord.js');
-const User = require('../../models/User');
-const { comma, coin } = require('../../utils/beatify')
-const { newCooldown, checkCooldown } = require('../../utils/cooldown')
-const errorHandler = require('../../utils/errorHandler')
+const { EmbedBuilder } = require("discord.js");
+const User = require("../../models/User");
+const { comma, coin } = require("../../utils/beatify");
+const { newCooldown, checkCooldown } = require("../../utils/cooldown");
+const errorHandler = require("../../utils/errorHandler");
 
 const dailyAmount = 1000;
 
 module.exports = {
-  name: 'daily',
-  description: 'Receive your daily reward of 1000',
-  blacklist: true,
-  
-  /**
-   *
-   * @param {Client} client
-   * @param {Interaction} interaction
-   */
-  callback: async (client, interaction) => {
-    try {
-      await interaction.deferReply();
-      const cooldownResult = await checkCooldown('daily', interaction, EmbedBuilder);
-      if (cooldownResult === 0) {
-        return;
-      }
+    name: "daily",
+    description: "Receive your daily reward of 1000",
+    blacklist: true,
 
-      const query = {
-        userId: interaction.member.id,
-      };
+    /**
+     *
+     * @param {Client} client
+     * @param {Interaction} interaction
+     */
+    callback: async (client, interaction) => {
+        try {
+            await interaction.deferReply();
+            const cooldownResult = await checkCooldown(
+                "daily",
+                interaction,
+                EmbedBuilder
+            );
+            if (cooldownResult === 0) {
+                return;
+            }
 
-      let user = await User.findOne(query);
+            const query = {
+                userId: interaction.member.id,
+            };
 
-      if (user) {
-        user.balance += dailyAmount;
-      } else {
-        user = new User({
-          ...query,
-          balance: dailyAmount
-        })
-      }
-      
-      await user.save();
+            let user = await User.findOne(query);
 
-      interaction.editReply(
-        { embeds : [ new EmbedBuilder()
-          .setTitle("Daily Reward")
-          .setDescription(`${comma(dailyAmount)} was added to your balance. Your new balance is ${coin(user.balance)}`)
-          .setColor("Yellow")
-        ]}
-      );
+            if (user) {
+                user.balance += dailyAmount;
+            } else {
+                user = new User({
+                    ...query,
+                    balance: dailyAmount,
+                });
+            }
 
-      await newCooldown('1d', interaction, 'daily')
-    }  catch (error) {
-			errorHandler(error, client, interaction, EmbedBuilder)
-		}
-  },
+            await user.save();
+
+            interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Daily Reward")
+                        .setDescription(
+                            `${comma(
+                                dailyAmount
+                            )} was added to your balance. Your new balance is ${coin(
+                                user.balance
+                            )}`
+                        )
+                        .setColor("Yellow"),
+                ],
+            });
+
+            await newCooldown("1d", interaction, "daily");
+        } catch (error) {
+            errorHandler(error, client, interaction, EmbedBuilder);
+        }
+    },
 };

38d47a8d8c26c0c7157ccc3c0b37193726350ef6
Yetity <hlonipoole692@gmail.com>
2023-06-18 12:00:29 +0200

    Sorted utils

---
+++ src/utils/handlers/cooldown.js
@@ -0,0 +1,117 @@
+const Cooldown = require("../../models/Cooldown");
+const strToMilli = require("../formatters/strToMilli");
+
+/**
+ *
+ * @param {String} time Can be a strin glike '2min' or '9d'
+ * @param {Interaction} interaction
+ * @param {String} name The command's name, if it is complex, set to the object name
+ * @param {String} complex If the name is complex like "challenge.lol" set name to "challenge" and this to "lol"
+ * @returns
+ */
+const newCooldown = async (time, interaction, name, complex = false) => {
+    let query = {
+        userId: interaction.user.id,
+    };
+    let date = Date.now();
+    let cooldown = await Cooldown.findOne(query);
+
+    if (complex === false || complex === undefined) {
+        if (cooldown && cooldown[name] && cooldown[name].getTime() > date) {
+            return;
+        }
+    } else {
+        if (
+            cooldown &&
+            cooldown[name][complex] &&
+            cooldown[name][complex].getTime() > date
+        ) {
+            return;
+        }
+    }
+
+    let cooldownTime;
+    if (typeof time == "string") {
+        cooldownTime = strToMilli(time);
+    } else if (typeof time === "number") {
+        cooldownTime = time;
+    } else {
+        throw `wtf is "${time}"`;
+    }
+
+    if (cooldown) {
+        complex === false || complex === undefined
+            ? (cooldown[name] = new Date(date + cooldownTime))
+            : (cooldown[name][complex] = new Date(date + cooldownTime));
+        await cooldown.save();
+    } else {
+        if (complex === false || complex === undefined) {
+            cooldown = new Cooldown({
+                ...query,
+                [name]: new Date(date + cooldownTime),
+            });
+        } else {
+            cooldown = new Cooldown({
+                ...query,
+                [name]: {
+                    [complex]: new Date(date + cooldownTime),
+                },
+            });
+        }
+        await cooldown.save();
+    }
+};
+
+/**
+ *
+ * @param {String} name The command's name.
+ * @param {Interaction} interaction
+ * @param {EmbedBuilder} EmbedBuilder
+ * @param {any} complex If the name is complex like "challenge.lol" set name to "challenge" and this to "lol"
+ * @param {String} custom Set's a custom cooldown message
+ * @returns
+ */
+const checkCooldown = async (
+    name,
+    interaction,
+    EmbedBuilder,
+    complex = false,
+    custom = null
+) => {
+    let query = {
+        userId: interaction.user.id,
+    };
+    let date = Date.now();
+    let description = ``;
+
+    let cooldown = await Cooldown.findOne(query);
+    let result =
+        complex === false || complex === undefined
+            ? cooldown[name]
+            : cooldown[name][complex];
+
+    if (cooldown && result) {
+        let remainingTime = result - date;
+        let endTime = Math.floor((Date.now() + remainingTime) / 1000);
+        if (custom !== null) {
+            description = `${custom} <t:${endTime}:R>`;
+        } else {
+            description = `Slow down bro. This command has a cooldown, you will be able to run this command <t:${endTime}:R>`;
+        }
+        if (remainingTime > 0) {
+            interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Cooldown")
+                        .setDescription(description)
+                        .setColor("Random"),
+                ],
+            });
+            return 0;
+        }
+    } else {
+        return;
+    }
+};
+
+module.exports = { newCooldown, checkCooldown };
--- src/utils/misc/badges/badges.js
+++ src/utils/misc/badges/badges.js
@@ -1,16 +1,15 @@
-const badgesJSON = require('./badges.json');
-const Badges = require('../../models/Badges');
-const {EmbedBuilder, Interaction} = require('discord.js')
+const badgesJSON = require("./badges.json");
+const Badges = require("../../../models/Badges");
 
 /**
- * 
+ *
  * @param {String} userId User's ID to give badge to
  * @param {String} badgeId Valid badge ID
- * 
+ *
  * Gives a user a badge without telling them. Will error if badge ID is not found in `./badges.json`
- * 
+ *
  * @returns True if badge has already been awarded.
- * 
+ *
  * Example :
  * ```
  * await giveBadge(interaction.user.id, devBadge)
@@ -20,65 +19,72 @@ const giveBadge = async (userId, badgeId) => {
     let badgesObj = Object.values(badgesJSON);
     let badgeIds = [];
 
-    for (let i = 0; i < badgesObj.length;i++) {
-        badgeIds.push(badgesObj[i].id)
-    };
+    for (let i = 0; i < badgesObj.length; i++) {
+        badgeIds.push(badgesObj[i].id);
+    }
 
     if (!badgeIds.includes(badgeId)) {
-        throw new Error(`Badge id (${badgeId}) not found!`)
-    };
+        throw new Error(`Badge id (${badgeId}) not found!`);
+    }
 
-    let badges = await Badges.findOne({userId: userId});
+    let badges = await Badges.findOne({ userId: userId });
 
     if (badges) {
         if (badges.badges[badgeId] == true) {
-            return true
+            return true;
         } else {
-            badges.badges[badgeId] = true
+            badges.badges[badgeId] = true;
         }
     } else {
         badges = new Badges({
             userId: userId.toString(),
             badges: {
-                [badgeId]: true
-            }
-        })
+                [badgeId]: true,
+            },
+        });
     }
 
     await badges.save();
-}
+};
 
 /**
- * 
+ *
  * @param {Interaction} interaction Pass interaction
  * @param {EmbedBuilder} EmbedBuilder Pass EmbedBuilder
  * @param {String} userId User's ID
  * @param {String} badgeId Badge ID
  * @param {Boolean} ephemeral Sets the ephemeral
- * 
+ *
  * NOTE: This does assume that you have replied to the interaction already, which is why it sends a follow up message.
- * 
- * Example usage. : 
+ *
+ * Example usage. :
  * ```
  * await awardBadge(interaction, EmbedBuilder, interaction.user.id, devBadge)
  * ```
  */
-const awardBadge = async (interaction, EmbedBuilder, userId, badgeId, ephemeral = false) => {
-    const a = await giveBadge(userId, badgeId)
-    if(a == true) {
-        return
+const awardBadge = async (
+    interaction,
+    EmbedBuilder,
+    userId,
+    badgeId,
+    ephemeral = false
+) => {
+    const a = await giveBadge(userId, badgeId);
+    if (a == true) {
+        return;
     } else {
-        
         await interaction.followUp({
-            embeds : [
+            embeds: [
                 new EmbedBuilder()
                     .setTitle("BADGE EARNED")
-                    .setDescription(`Congrats you just earned the **${badgesJSON[badgeId].name}** badge!`)
-                    .setColor("Yellow")
+                    .setDescription(
+                        `Congrats you just earned the **${badgesJSON[badgeId].name}** badge!`
+                    )
+                    .setColor("Yellow"),
             ],
-            ephemeral: ephemeral
-        })
+            ephemeral: ephemeral,
+        });
     }
-}
+};
 
-module.exports = {giveBadge, awardBadge}
+module.exports = { giveBadge, awardBadge };

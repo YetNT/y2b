cbb04a2df9d99d4f10e19625879f5a47e0803e9e
Yetity <hlonipoole692@gmail.com>
2023-06-18 17:51:39 +0200

    Added pager to shop

--- src/commands/economy-get/shop.js
+++ src/commands/economy-get/shop.js
@@ -1,7 +1,13 @@
-const { EmbedBuilder } = require("discord.js");
+const {
+    EmbedBuilder,
+    ComponentType,
+    ButtonBuilder,
+    ButtonStyle,
+} = require("discord.js");
 const items = require("../../utils/misc/items/items.json");
 const { shopify } = require("../../utils/formatters/beatify");
 const errorHandler = require("../../utils/handlers/errorHandler");
+const { pageCreator, pagerButtons } = require("../../utils/handlers/pages");
 
 module.exports = {
     name: "shop",
@@ -11,21 +17,129 @@ module.exports = {
     callback: async (client, interaction) => {
         try {
             await interaction.deferReply();
-            let reply = ``;
+            let nextPage = new ButtonBuilder()
+                .setCustomId("nextPage")
+                .setLabel("Next Page")
+                .setStyle(ButtonStyle.Secondary);
+            let previousPage = new ButtonBuilder()
+                .setCustomId("prevPage")
+                .setLabel("Previous Page")
+                .setStyle(ButtonStyle.Secondary);
+            let reply = [];
 
             for (let item in items) {
-                reply += `__${items[item].name}__ - ${shopify(
-                    items[item].price
-                )}\n${items[item].description}\n\n`;
+                reply.push(
+                    `__${items[item].name}__ - ${shopify(items[item].price)}\n${
+                        items[item].description
+                    }`
+                );
             }
 
-            await interaction.editReply({
+            let pageItems = pageCreator(reply, 5);
+            let embed = new EmbedBuilder().setTitle("Shop").setColor("Fuchsia");
+            let initRow =
+                pageItems.length != 1
+                    ? [pagerButtons(nextPage, previousPage, "previous")]
+                    : [];
+
+            var pageIndex = 1;
+            const response = await interaction.editReply({
                 embeds: [
-                    new EmbedBuilder()
-                        .setTitle("Shop")
-                        .setDescription(reply)
-                        .setColor("Fuchsia"),
+                    embed.setDescription(reply.join("\n\n")).setFooter({
+                        text: `Page ${pageIndex}/${pageItems.length}`,
+                    }),
                 ],
+                components: initRow,
+            });
+
+            const collector = response.createMessageComponentCollector({
+                componentType: ComponentType.Button,
+                time: 3_600_000,
+            });
+
+            collector.on("collect", async (i) => {
+                let id = i.customId;
+
+                if (id == "prevPage") {
+                    pageIndex--;
+                    if (pageIndex != 1) {
+                        let pagerRow = pagerButtons(
+                            nextPage,
+                            previousPage,
+                            null
+                        );
+                        await i.update({
+                            embeds: [
+                                embed
+                                    .setDescription(
+                                        pageItems[pageIndex - 1].join("\n\n")
+                                    )
+                                    .setFooter({
+                                        text: `Page ${pageIndex}/${pageItems.length}`,
+                                    }),
+                            ],
+                            components: [pagerRow],
+                        });
+                    } else {
+                        let pagerRow = pagerButtons(
+                            nextPage,
+                            previousPage,
+                            "previous"
+                        );
+                        await i.update({
+                            embeds: [
+                                embed
+                                    .setDescription(
+                                        pageItems[pageIndex - 1].join("\n\n")
+                                    )
+                                    .setFooter({
+                                        text: `Page ${pageIndex}/${pageItems.length}`,
+                                    }),
+                            ],
+                            components: [pagerRow],
+                        });
+                    }
+                }
+                if (id == "nextPage") {
+                    pageIndex++;
+                    if (pageIndex != pageItems.length) {
+                        let pagerRow = pagerButtons(
+                            nextPage,
+                            previousPage,
+                            null
+                        );
+                        await i.update({
+                            embeds: [
+                                embed
+                                    .setDescription(
+                                        pageItems[pageIndex - 1].join("\n\n")
+                                    )
+                                    .setFooter({
+                                        text: `Page ${pageIndex}/${pageItems.length}`,
+                                    }),
+                            ],
+                            components: [pagerRow],
+                        });
+                    } else {
+                        let pagerRow = pagerButtons(
+                            nextPage,
+                            previousPage,
+                            "next"
+                        );
+                        await i.update({
+                            embeds: [
+                                embed
+                                    .setDescription(
+                                        pageItems[pageIndex - 1].join("\n\n")
+                                    )
+                                    .setFooter({
+                                        text: `Page ${pageIndex}/${pageItems.length}`,
+                                    }),
+                            ],
+                            components: [pagerRow],
+                        });
+                    }
+                }
             });
         } catch (error) {
             errorHandler(error, client, interaction, EmbedBuilder);

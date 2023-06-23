bbe97b943d96927a0109fb843b761f634d96f01e
Yetity <hlonipoole692@gmail.com>
2023-06-23 18:51:09 +0200

    Prettier

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,20 +1,33 @@
-const path = require('path');
-const getAllFiles = require('../utils/getAllFiles');
+const path = require("path");
+const getAllFiles = require("../utils/getAllFiles");
 
 module.exports = (client) => {
-  const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true);
+    const eventFolders = getAllFiles(
+        // eslint-disable-next-line no-undef
+        path.join(__dirname, "..", "events"),
+        true
+    );
 
-  for (const eventFolder of eventFolders) {
-    const eventFiles = getAllFiles(eventFolder);
-    eventFiles.sort((a, b) => a > b);
+    for (const eventFolder of eventFolders) {
+        const eventFiles = getAllFiles(eventFolder);
+        eventFiles.sort((a, b) => a > b);
 
-    const eventName = eventFolder.replace(/\\/g, '/').split('/').pop();
+        const eventName = eventFolder.replace(/\\/g, "/").split("/").pop();
 
-    client.on(eventName, async (arg) => {
-      for (const eventFile of eventFiles) {
-        const eventFunction = require(eventFile);
-        await eventFunction(client, arg);
-      }
-    });
-  }
+        client.on(eventName, async (arg) => {
+            for (const eventFile of eventFiles) {
+                const eventFunction = require(eventFile);
+                if (eventFile.includes("dbPost") == true) {
+                    if (client.application.id == "701280304182067251") {
+                        await eventFunction(client, arg);
+                        console.log("dbPost has been loaded");
+                    } else {
+                        continue;
+                    }
+                } else {
+                    await eventFunction(client, arg);
+                }
+            }
+        });
+    }
 };

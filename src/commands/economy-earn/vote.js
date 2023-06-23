03f5a59ab12dd7691d053ab2c486845fe4ffe42c
Yetity <hlonipoole692@gmail.com>
2023-06-23 19:18:25 +0200

    Vote command

---
+++ src/commands/economy-earn/vote.js
@@ -0,0 +1,51 @@
+const errorHandler = require("../../utils/handlers/errorHandler");
+const { coin } = require("../../utils/formatters/beatify");
+const {
+    EmbedBuilder,
+    ButtonBuilder,
+    ActionRowBuilder,
+    ButtonStyle,
+} = require("discord.js");
+
+const links = {
+    dbl: "https://discordbotlist.com/bots/yet-20/upvote",
+    top: "https://top.gg/bot/701280304182067251/vote",
+};
+
+let amt = 10_000;
+
+module.exports = {
+    name: "vote",
+    description: "Vote for the bot on top.gg and discordbotlist",
+
+    callback: async (client, interaction) => {
+        try {
+            await interaction.deferReply();
+
+            const top = new ButtonBuilder()
+                .setLabel("Top.gg")
+                .setStyle(ButtonStyle.Link)
+                .setURL(links.top);
+            const dbl = new ButtonBuilder()
+                .setLabel("Discordbotlist")
+                .setStyle(ButtonStyle.Link)
+                .setURL(links.dbl);
+
+            const row = new ActionRowBuilder().addComponents(top, dbl);
+            await interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Vote for the bot!")
+                        .setDescription(
+                            `Thanks for supporting the bot :)\nIn return you'll receive ${coin(
+                                amt
+                            )}\n\nWait for at least **10mins** before reporting that you didn't receive your rewards.`
+                        ),
+                ],
+                components: [row],
+            });
+        } catch (error) {
+            errorHandler(error, client, interaction, EmbedBuilder);
+        }
+    },
+};

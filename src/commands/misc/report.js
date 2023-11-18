02575e0c62bd801fe9f78a7498d8bc5cc72b3d71
Yetity <hlonipoole692@gmail.com>
2023-11-18 06:35:10 +0200

    report

--- src/commands/misc/report.js
+++ src/commands/misc/report.js
@@ -1,20 +1,20 @@
-const errorHandler = require("../../utils/handlers/errorHandler")
-const { EmbedBuilder } = require("discord.js")
+const errorHandler = require("../../utils/handlers/errorHandler");
+const { EmbedBuilder } = require("discord.js");
 
-const subcommands = require("./reportSubcommands/index")
+const subcommands = require("./reportSubcommands/index");
 
 function arrToObj(originalArray) {
     const transformedObject = {};
 
-    originalArray.forEach(item => {
+    originalArray.forEach((item) => {
         transformedObject[item.name] = {
             id: item.id,
             name: item.name,
             moderated: item.moderated,
             emoji: {
                 id: item.emoji.id,
-                name: item.emoji.name
-            }
+                name: item.emoji.name,
+            },
         };
     });
 
@@ -22,37 +22,56 @@ function arrToObj(originalArray) {
 }
 
 module.exports = {
-	name:"report",
-	description: "Report stuff (Contains subcommands)",
-	options: [subcommands.bug.body, subcommands.user.body, subcommands.server.body],
-	blacklist: true,
+    name: "report",
+    description: "Report stuff (Contains subcommands)",
+    options: [
+        subcommands.bug.body,
+        subcommands.user.body,
+        subcommands.server.body,
+    ],
+    blacklist: true,
 
-	callback: async (client, interaction) => {
-		await interaction.deferReply();
-		try {
-			const forum = client.channels.cache.get("1172574488362242098")
-			const tagsObj = arrToObj(forum.availableTags)
-			
-			const tags = {
-				"bug":tagsObj.Bug.id,
-				"server":tagsObj.server.id,
-				"user":tagsObj.user.id
-			}
-			
-			let subcommand = interaction.options.getSubcommand()
-			switch (subcommand) {
-				case "bug":
-					await subcommands.bug.callback(client, interaction, forum, tags)
-					break;
-				case "user":
-					await subcommands.user.callback(client, interaction, forum, tags)
-					break;
-				case "server":
-					await subcommands.server.callback(client, interaction, forum, tags)
-					break;
-			}
-		} catch (error) {
-			errorHandler(error, client, interaction, EmbedBuilder)
-		}
-	}
-}
+    callback: async (client, interaction) => {
+        await interaction.deferReply({ ephemeral: true });
+        try {
+            const forum = client.channels.cache.get("1172574488362242098");
+            const tagsObj = arrToObj(forum.availableTags);
+
+            const tags = {
+                bug: tagsObj.Bug.id,
+                server: tagsObj.server.id,
+                user: tagsObj.user.id,
+            };
+
+            let subcommand = interaction.options.getSubcommand();
+            switch (subcommand) {
+                case "bug":
+                    await subcommands.bug.callback(
+                        client,
+                        interaction,
+                        forum,
+                        tags
+                    );
+                    break;
+                case "user":
+                    await subcommands.user.callback(
+                        client,
+                        interaction,
+                        forum,
+                        tags
+                    );
+                    break;
+                case "server":
+                    await subcommands.server.callback(
+                        client,
+                        interaction,
+                        forum,
+                        tags
+                    );
+                    break;
+            }
+        } catch (error) {
+            errorHandler(error, client, interaction, EmbedBuilder);
+        }
+    },
+};

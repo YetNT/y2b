070de20d7ca6dd10328bafa4439c61d3e696365f
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-12-03 09:58:47 +0200

    better strToMilli

--- src/utils/formatters/strToMilli.js
+++ src/utils/formatters/strToMilli.js
@@ -2,10 +2,12 @@
  * @param {String} input Input
  */
 module.exports = (input) => {
-    var ar = input.match(/[a-zA-Z]+|[0-9]+/g);
-    var value = +ar[0];
-    var unit = ar[1];
-  
+    let ar = input.match(/([0-9]*\.?[0-9]+)([a-zA-Z]+)/);
+    if (!ar) return 0;
+
+    let value = parseFloat(ar[1]);
+    let unit = ar[2].toLowerCase();
+
     switch (unit) {
         case "s":
         case "seconds":
@@ -17,20 +19,56 @@ module.exports = (input) => {
         case "h":
         case "hour":
         case "hours":
-            return value * 3.6e+6;
+            return value * 3.6e6;
         case "d":
         case "day":
         case "days":
-            return value * 8.64e+7;
+            return value * 8.64e7;
         case "w":
         case "week":
         case "weeks":
-            return value * 6.048e+8;
+            return value * 6.048e8;
         case "m":
         case "month":
         case "months":
-            return value * 2.628e+9;
+            return value * 2.628e9;
         default:
             return 0; // Invalid input, return 0 or handle the error as desired
     }
 };
+
+function lol(input) {
+    var ar = input.match(/([0-9]*\.?[0-9]+)([a-zA-Z]+)/);
+    if (!ar) return 0;
+
+    var value = parseFloat(ar[1]);
+    var unit = ar[2].toLowerCase();
+
+    switch (unit) {
+        case "s":
+        case "seconds":
+            return value * 1000;
+        case "min":
+        case "minutes":
+        case "mins":
+            return value * 60000;
+        case "h":
+        case "hour":
+        case "hours":
+            return value * 3.6e6;
+        case "d":
+        case "day":
+        case "days":
+            return value * 8.64e7;
+        case "w":
+        case "week":
+        case "weeks":
+            return value * 6.048e8;
+        case "m":
+        case "month":
+        case "months":
+            return value * 2.628e9;
+        default:
+            return 0;
+    }
+}
